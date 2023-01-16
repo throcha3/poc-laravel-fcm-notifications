f09c050018ea3d272d4d6a2948f6e78a19059bab
Thiago <throcha3@gmail.com>
2023-01-15 21:12:01 -0300

    Send notification for device or topics

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\{HomeController, NotificationSendController};
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,4 +19,11 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::group(['middleware' => 'auth'],function(){
+    Route::post('/store-token', [NotificationSendController::class, 'updateDeviceToken'])->name('store.token');
+    Route::post('/send-web-notification', [NotificationSendController::class, 'sendNotificationToDevice'])->name('send.web-notification');
+});
+
+Route::get('/send-topic-notification', [NotificationSendController::class, 'sendNotificationToTopic'])->name('send.topic-notification');
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
